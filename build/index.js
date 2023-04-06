4fc2dc13f83c6436accba3ecf5126e5c298306c9
Andrew Buter <andrey.buter@gmail.com>
2023-04-06 23:06:12 +0200

    Increase maxListeners amoun
    (node:29311) MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 close listeners added to [TLSSocket]. Use emitter.setMaxListeners() to increase limit

--- build/index.js
+++ build/index.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const telegram_1 = require("./telegram");
+// https://stackoverflow.com/questions/8313628/node-js-request-how-to-emitter-setmaxlisteners
+require('events').EventEmitter.prototype._maxListeners = 100;
 new telegram_1.TelegramBotMessageHandler();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { TelegramBotMessageHandler } from './telegram';
 
+// https://stackoverflow.com/questions/8313628/node-js-request-how-to-emitter-setmaxlisteners
+require('events').EventEmitter.prototype._maxListeners = 100;
+
 new TelegramBotMessageHandler();
