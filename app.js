f2c893bf2f7740a7d00d79740c79dd5b0af66ffe
Andrew Buter <andrey.buter@gmail.com>
2023-04-06 23:37:49 +0200

    upd app js

--- app.js
+++ app.js
@@ -2,27 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app = require("./build/index.js");
 
-// var http = require('http');
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     var message = 'It works!\n',
-//         version = 'NodeJS ' + process.versions.node + '\n',
-//         response = [message, version].join('\n');
-//     res.end(response);
-// });
-// server.listen();
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
 
 // https://docs.cpanel.net/knowledge-base/web-services/how-to-install-a-node.js-application/
-const http = require("http");
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World! NodeJS \n");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const http = require("http");
+// const hostname = "127.0.0.1";
+// const port = 3000;
+//
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World! NodeJS \n");
+// });
+//
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
